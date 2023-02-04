44a2d3e38d2d8da47c5fa46f21324a95aa7e66a7
Mark Owings <owingsmk@mail.uc.edu>
2023-02-04 13:37:06 -0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("What is your Credit card balance? ");
+        int creditCardBalance = 5000;
+        System.out.print("What is the interest rate? ");
+        double interestRate = .17;
+        int totalInterest;
+        int totalDue;
+        int monthlyPayment;
+        totalInterest = creditCardBalance * interestRate;
+        int monthPayment = 1;
+
+        if monthlyPayment < 5000 + monthPayment;
+        totalDue = creditCardBalance + totalInterest;
+
+
     }
 }
