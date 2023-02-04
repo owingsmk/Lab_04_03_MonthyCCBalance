4a8ffeb51e77856fa9a9b9086494b66e802854bb
Mark Owings <owingsmk@mail.uc.edu>
2023-02-04 13:51:36 -0500

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("What is your Credit card balance? ");
-        int creditCardBalance = 5000;
-        System.out.print("What is the interest rate? ");
-        double interestRate = .17;
+
         double totalInterest;
         double totalDue;
         double secondMonth;
-        totalInterest = creditCardBalance * interestRate;
-        totalDue = creditCardBalance + totalInterest;
-        System.out.print("This is how much you owe on the First month "+ totalDue);
+
+
+        System.out.println("What is your Credit card balance? ");
+            int creditCardBalance = 5000; //What the user would input
+        System.out.print("What is the interest rate? ");
+            double interestRate = 0.17;
+
+        totalInterest = creditCardBalance * interestRate; //Calculation the interest for the current balance
+            System.out.print("This is how much you owe on the First month " + totalDue);
+        totalDue = creditCardBalance + totalInterest; //Calculation if they user misses the first payment and has to pay more on the second month.
+            System.out.print("You have missed your second months payment ");
         secondMonth = totalDue * totalInterest;
-        System.out.print("Your total due on the second month is " + secondMonth);
+            System.out.print("Your total due on the second month is " + secondMonth);
 
 
     }
